7649fc044e2eac733e6e0dac2f4e826ff2beb831
Nemanja Makuljevic <nemanjamakuljevic@Nemanjas-MacBook-Air.local>
2023-02-02 01:11:06 +0100

    Adding test_posts-GET

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from app.main import app
 from app.config import settings
 from app.database import get_db, Base
+from app.oauth2 import create_access_token
+from app import models
 
 
 # creating testing database
@@ -54,3 +56,48 @@ def test_user(client):
     new_user['password'] = user_data['password']
     return new_user
 
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [
+        {
+            "title": "first post",
+            "content": "first content",
+            "owner_id": test_user['id']
+        },
+        {
+            "title": "2nd post",
+            "content": "2nd content",
+            "owner_id": test_user['id']
+        },
+        {
+            "title": "3rd post",
+            "content": "3rd content",
+            "owner_id": test_user['id']
+        }
+    ]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    # session.add_all([models.Post(title="first post", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd post", content="2nd content", owner_id=test_user['id']),
+    #                 models.Post(title="3rd post", content="3rd content", owner_id=test_user['id'])])
+    session.commit()
+    posts = session.query(models.Post).all()
+    return posts
