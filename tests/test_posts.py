aa16b7f7db94538fc1440fc00dc8d249c4c23485
Nemanja Makuljevic <nemanjamakuljevic@Nemanjas-MacBook-Air.local>
2023-05-10 19:59:55 +0200

    Updating test_posts with UPDATE post tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -80,3 +80,38 @@ def test_delete_non_existing_post(authorized_client, test_user, test_posts):
 def test_delete_other_user_post(authorized_client, test_user, test_posts):
     res = authorized_client.delete(f"/posts/{test_posts[3].id}")
     assert res.status_code == 403
+
+#UPDATE post test
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated test title test", 
+        "content": "updated test content test",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "updated test title test", 
+        "content": "updated test content test",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+def test_unauthorized_user_update_post(client, test_posts, test_user):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_update_non_existing_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated test title test", 
+        "content": "updated test content test",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/90090908980909809099809809809009", json=data)
+    assert res.status_code == 404
